0c7a26e6573166167e1a20dde7b022a2aa5de73b
Julius Ware <jcware8@gmail.com>
2014-11-20 10:18:50 -0500

    These are the updated datastructure file and test file for Assignmnet 5

--- Assignment5/src/Assignment5.cpp
+++ Assignment5/src/Assignment5.cpp
@@ -10,18 +10,47 @@ using namespace ods;
 
 int main() {
 	DataStructureTester<BinarySearchTree<BSTNode1<int>, int> > test;
-
-	//test.AddRandom(10);
-
-	test.DoRandomAdd(100000);
-	//test.DoSequentialAdd(0, 100000, 1);
-
 	DataStructureTester<RedBlackTree<RedBlackNode1<int>, int> > test2;
-	//test2.DoRandomAdd(100000);
-	//test2.DoSequentialAdd(0, 100000, 1);
+	DataStructureTester<LinearHashTable<int> > test3;
+	DataStructureTester<ChainedHashTable<int> > test4;
 
-	//DataStructureTester<LinearHashTable<int>(0,-1)> test3;
-	//test3.DoSequentialAdd(0, 100000, 1);
+//BStree test
 
+	for(int i = 0; i < 20; i++){
+	cout << "BST Sequential add" << i+1 << ":" << test.DoSequentialAdd(0, 50000, 1) << endl;
+	cout << "BST Random add" << i+1 << ":" << test.DoRandomAdd(50000) << endl;
+	cout << "BST SeqFind" << i+1 << ":" << test.DoSequentialFind(0, 50000, 1) << endl;
+	cout << "BST RandFind" << i+1 << ":" << test.DoRandomFind(50000) << endl;
+	cout << "BST SeqRem" << i+1 << ":" << test.DoSequentialRemove(0, 50000, 1) << endl;
+	cout << "BST RandRem" << i+1 << ":" << test.DoRandomRemove(50000) << "\n"<< endl;
+
+//RBtree test
+	cout << "RBTree Sequential add" << i+1 << ":" << test2.DoSequentialAdd(0, 50000, 1) << endl;
+	cout << "RBTree Random add" << i+1 << ":" << test2.DoRandomAdd(50000) << endl;
+	cout << "RBTree SeqFind" << i+1 << ":" << test2.DoSequentialFind(0, 50000, 1) << endl;
+	cout << "RBTree RandFind" << i+1 << ":" << test2.DoRandomFind(50000) << endl;
+	cout << "RBTree SeqRem" << i+1 << ":" << test2.DoSequentialRemove(0, 50000, 1) << endl;
+	cout << "RBTree RandRem" << i+1 << ":" << test2.DoRandomRemove(50000) << "\n"<< endl;
+
+
+//LHashTable test
+		cout << "LHT Sequential add" << i+1 << ":" << test3.DoSequentialAdd(0, 50000, 1) << endl;
+		cout << "LHT Random add" << i+1 << ":" << test3.DoRandomAdd(50000) << endl;
+		cout << "LHT SeqFind" << i+1 << ":" << test3.DoSequentialFind(0, 50000, 1) << endl;
+		cout << "LHT RandFind" << i+1 << ":" << test3.DoRandomFind(50000) << endl;
+		cout << "LHT SeqRem" << i+1 << ":" << test3.DoSequentialRemove(0, 50000, 1) << endl;
+		cout << "LHT RandRem" << i+1 << ":" << test3.DoRandomRemove(50000) << "\n"<< endl;
+
+
+//CHashTable test
+		cout << "CHT Sequential add" << i+1 << ":" << test4.DoSequentialAdd(0, 50000, 1) << endl;
+		cout << "CHT Random add" << i+1 << ":" << test4.DoRandomAdd(50000) << endl;
+		cout << "CHT SeqFind" << i+1 << ":" << test4.DoSequentialFind(0, 50000, 1) << endl;
+		cout << "CHT RandFind" << i+1 << ":" << test4.DoRandomFind(50000) << endl;
+		cout << "CHT SeqRem" << i+1 << ":" << test4.DoSequentialRemove(0, 50000, 1) << endl;
+		cout << "CHT RandRem" << i+1 << ":" << test4.DoRandomRemove(50000) << "\n"<< endl;
+
+	}
 	return 0;
+
 }
--- Assignment5/src/DataStructureTester.h
+++ Assignment5/src/DataStructureTester.h
@@ -11,35 +11,31 @@ protected:
 	float timer;
 
 public:
-	void AddRandom(int i) {
-		list.add(i);
-		std::cout << "find " << list.find(i) << std::endl;
-	}
 
 	int xRand;
 	int DoSequentialAdd(int start, int end, int step);
 	int DoRandomAdd(int n);
 	int DoSequentialRemove(int start, int end, int step);
 	int DoRandomRemove(int n);
+	int DoSequentialFind(int start, int end, int step);
+	int DoRandomFind(int n);
 
 };
 
 template<class H>
 int DataStructureTester<H>::DoSequentialAdd(int start, int end, int step) {
+
 	timer = clock();
 	for (int i = start; i < end; i = i + step) {
 
-		// what do i do here?
-		//add items;
 		list.add(i);
-		std::cout << "find " << list.find(i) << std::endl;
+
 	}
 
 	timer = clock() - timer;
-	timer = (((float) timer) / CLOCKS_PER_SEC);
-	std::cout << "took " << timer << " seconds" << std::endl;
+	timer = 1000 * (((float) timer) / CLOCKS_PER_SEC);
 
-	return 0;
+	return timer;
 
 }
 
@@ -50,21 +46,81 @@ int DataStructureTester<H>::DoRandomAdd(int n) {
 	for (int i = 0; i < n; i++) {
 		xRand = rand() % n + 1;
 		list.add(xRand);
-		std::cout << "find " << list.find(xRand) << std::endl;
-		// how to i add to generic structure??
-		// add xRand
 
 	}
 
 	timer = clock() - timer;
-	timer = (((float) timer) / CLOCKS_PER_SEC);
-	std::cout << "took " << timer << " seconds" << std::endl;
+	timer = 1000 * (((float) timer) / CLOCKS_PER_SEC);
 
-	return 0;
+	return timer;
 }
-
 
 template<class H>
-int DataStructureTester<H>::DoSequentialRemove(int n)
+int DataStructureTester<H>::DoSequentialRemove(int start, int end, int step) {
+
+	timer = clock();
+	for (int i = start; i < end; i = i + step) {
+
+		list.remove(i);
+
+	}
+
+	timer = clock() - timer;
+	timer = 1000 * (((float) timer) / CLOCKS_PER_SEC);
+
+	return timer;
+}
+
+template<class H>
+int DataStructureTester<H>::DoRandomRemove(int n) {
+	timer = clock();
+	srand(time(0));
+	for (int i = 0; i < n; i++) {
+		xRand = rand() % n + 1;
+		list.remove(xRand);
+
+	}
+
+	timer = clock() - timer;
+	timer = 1000 * (((float) timer) / CLOCKS_PER_SEC);
+	;
+
+	return timer;
+}
+
+template<class H>
+int DataStructureTester<H>::DoSequentialFind(int start, int end, int step) {
+
+	timer = clock();
+	for (int i = start; i < end; i = i + step) {
+
+		list.find(i);
+
+	}
+
+	timer = clock() - timer;
+	timer = 1000 * (((float) timer) / CLOCKS_PER_SEC);
+
+	return timer;
 
 }
+
+template<class H>
+int DataStructureTester<H>::DoRandomFind(int n) {
+
+	timer = clock();
+	srand(time(0));
+	for (int i = 0; i < n; i++) {
+		xRand = rand() % n + 1;
+		list.find(xRand);
+
+	}
+
+	timer = clock() - timer;
+	timer = 1000 * (((float) timer) / CLOCKS_PER_SEC);
+
+	return timer;
+}
+
+}
+
