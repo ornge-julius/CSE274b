23a72cfc8ec75f6fbc97b4fcb0b3e6862fcd2776
Julius Ware <jcware8@gmail.com>
2014-11-20 22:35:00 -0500

    This commit contains the updated testerfile 'Assignnment.cpp' and also includes a txt file that includes the data from the tests. There is also included a summary of the different data structures tested in the program.

--- Assignment5/src/Assignment5.cpp
+++ Assignment5/src/Assignment5.cpp
@@ -14,43 +14,95 @@ int main() {
 	DataStructureTester<LinearHashTable<int> > test3;
 	DataStructureTester<ChainedHashTable<int> > test4;
 
+//These are holders for the sums of the total number of milliseconds for each test
+
+	float t1 = 0, t2 = 0, t3 = 0, t4 = 0, t5 = 0, t6 = 0;
+
 //BStree test
 
-	for(int i = 0; i < 20; i++){
-	cout << "BST Sequential add" << i+1 << ":" << test.DoSequentialAdd(0, 50000, 1) << endl;
-	cout << "BST Random add" << i+1 << ":" << test.DoRandomAdd(50000) << endl;
-	cout << "BST SeqFind" << i+1 << ":" << test.DoSequentialFind(0, 50000, 1) << endl;
-	cout << "BST RandFind" << i+1 << ":" << test.DoRandomFind(50000) << endl;
-	cout << "BST SeqRem" << i+1 << ":" << test.DoSequentialRemove(0, 50000, 1) << endl;
-	cout << "BST RandRem" << i+1 << ":" << test.DoRandomRemove(50000) << "\n"<< endl;
+//Runs each test 20 times and sums up the times
+	for (int i = 0; i < 20; i++) {
 
-//RBtree test
-	cout << "RBTree Sequential add" << i+1 << ":" << test2.DoSequentialAdd(0, 50000, 1) << endl;
-	cout << "RBTree Random add" << i+1 << ":" << test2.DoRandomAdd(50000) << endl;
-	cout << "RBTree SeqFind" << i+1 << ":" << test2.DoSequentialFind(0, 50000, 1) << endl;
-	cout << "RBTree RandFind" << i+1 << ":" << test2.DoRandomFind(50000) << endl;
-	cout << "RBTree SeqRem" << i+1 << ":" << test2.DoSequentialRemove(0, 50000, 1) << endl;
-	cout << "RBTree RandRem" << i+1 << ":" << test2.DoRandomRemove(50000) << "\n"<< endl;
-
-
-//LHashTable test
-		cout << "LHT Sequential add" << i+1 << ":" << test3.DoSequentialAdd(0, 50000, 1) << endl;
-		cout << "LHT Random add" << i+1 << ":" << test3.DoRandomAdd(50000) << endl;
-		cout << "LHT SeqFind" << i+1 << ":" << test3.DoSequentialFind(0, 50000, 1) << endl;
-		cout << "LHT RandFind" << i+1 << ":" << test3.DoRandomFind(50000) << endl;
-		cout << "LHT SeqRem" << i+1 << ":" << test3.DoSequentialRemove(0, 50000, 1) << endl;
-		cout << "LHT RandRem" << i+1 << ":" << test3.DoRandomRemove(50000) << "\n"<< endl;
-
-
-//CHashTable test
-		cout << "CHT Sequential add" << i+1 << ":" << test4.DoSequentialAdd(0, 50000, 1) << endl;
-		cout << "CHT Random add" << i+1 << ":" << test4.DoRandomAdd(50000) << endl;
-		cout << "CHT SeqFind" << i+1 << ":" << test4.DoSequentialFind(0, 50000, 1) << endl;
-		cout << "CHT RandFind" << i+1 << ":" << test4.DoRandomFind(50000) << endl;
-		cout << "CHT SeqRem" << i+1 << ":" << test4.DoSequentialRemove(0, 50000, 1) << endl;
-		cout << "CHT RandRem" << i+1 << ":" << test4.DoRandomRemove(50000) << "\n"<< endl;
+		t1 = t1 + test.DoSequentialAdd(0, 50000, 1);
+		t2 = t2 + test.DoRandomAdd(50000);
+		t3 = t3 + test.DoSequentialFind(0, 50000, 1);
+		t4 = t3 + test.DoRandomFind(50000);
+		t5 = t5 + test.DoSequentialRemove(0, 50000, 1);
+		t6 = t6 + test.DoRandomRemove(50000);
 
 	}
+
+//Prints out the averages of each test then resets the holders
+	cout << t1 / 20 << endl;
+	cout << t2 / 20 << endl;
+	cout << t3 / 20 << endl;
+	cout << t4 / 20 << endl;
+	cout << t5 / 20 << endl;
+	cout << t6 / 20 << "\n" << endl;
+	t1 = 0, t2 = 0, t3 = 0, t4 = 0, t5 = 0, t6 = 0;
+
+//RBtree test
+
+	for (int i = 0; i < 20; i++) {
+
+		t1 = t1 + test2.DoSequentialAdd(0, 50000, 1);
+		t2 = t2 + test2.DoRandomAdd(50000);
+		t3 = t3 + test2.DoSequentialFind(0, 50000, 1);
+		t4 = t3 + test2.DoRandomFind(50000);
+		t5 = t5 + test2.DoSequentialRemove(0, 50000, 1);
+		t6 = t6 + test2.DoRandomRemove(50000);
+
+	}
+
+	cout << t1 / 20 << endl;
+	cout << t2 / 20 << endl;
+	cout << t3 / 20 << endl;
+	cout << t4 / 20 << endl;
+	cout << t5 / 20 << endl;
+	cout << t6 / 20 << "\n" << endl;
+	t1 = 0, t2 = 0, t3 = 0, t4 = 0, t5 = 0, t6 = 0;
+
+//LHashTable test
+
+	for (int i = 0; i < 20; i++) {
+
+		t1 = t1 + test3.DoSequentialAdd(0, 50000, 1);
+		t2 = t2 + test3.DoRandomAdd(50000);
+		t3 = t3 + test3.DoSequentialFind(0, 50000, 1);
+		t4 = t3 + test3.DoRandomFind(50000);
+		t5 = t5 + test3.DoSequentialRemove(0, 50000, 1);
+		t6 = t6 + test3.DoRandomRemove(50000);
+
+	}
+
+	cout << t1 / 20 << endl;
+	cout << t2 / 20 << endl;
+	cout << t3 / 20 << endl;
+	cout << t4 / 20 << endl;
+	cout << t5 / 20 << endl;
+	cout << t6 / 20 << "\n" << endl;
+	t1 = 0, t2 = 0, t3 = 0, t4 = 0, t5 = 0, t6 = 0;
+
+//CHashTable test
+
+	for (int i = 0; i < 20; i++) {
+
+		t1 = t1 + test4.DoSequentialAdd(0, 50000, 1);
+		t2 = t2 + test4.DoRandomAdd(50000);
+		t3 = t3 + test4.DoSequentialFind(0, 50000, 1);
+		t4 = t3 + test4.DoRandomFind(50000);
+		t5 = t5 + test4.DoSequentialRemove(0, 50000, 1);
+		t6 = t6 + test4.DoRandomRemove(50000);
+
+	}
+
+	cout << t1 / 20 << endl;
+	cout << t2 / 20 << endl;
+	cout << t3 / 20 << endl;
+	cout << t4 / 20 << endl;
+	cout << t5 / 20 << endl;
+	cout << t6 / 20 << "\n" << endl;
+
 	return 0;
 
 }
